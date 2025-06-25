ae6a244bb135c8177034780e76b3273604717892
alejandrodc7 <alejandro.d.cardenas@gmail.com>
2025-06-25 18:41:53 -0500

    algoritmo de seleccion en lista ordenada

---
+++ seleccion.js
@@ -0,0 +1,16 @@
+let ejemplo = [1,5,3,11,7,9]
+for(let j = 0;j<ejemplo.length; j++){
+let datoMin = ejemplo[j]
+let posMin = j
+for( let i = j; i<ejemplo.length; i++){
+    if(datoMin>ejemplo[i+1]){
+        datoMin = ejemplo[i+1]
+        posMin = i+1
+        console.log("dato:",datoMin,"pos:",posMin)
+    }
+}
+console.log("dato fin:",datoMin,"pos fin:",posMin)
+ejemplo[posMin] = ejemplo[j] 
+ejemplo[j] = datoMin
+console.log(ejemplo)
+}
